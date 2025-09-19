901ae756eaa804aa642aa52fada24ccebc34b1af
Bilal Ibrahimov <ibrahimovbilal9@gmail.com>
2025-09-19 15:24:21 +0400

    baki vaxti ile

--- src/main/java/com/example/DebitCopybook/DebitCopybookApplication.java
+++ src/main/java/com/example/DebitCopybook/DebitCopybookApplication.java
@@ -1,11 +1,19 @@
 package com.example.DebitCopybook;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class DebitCopybookApplication {
 
+	@PostConstruct
+	public void init() {
+		// Tətbiqin işlədiyi hər yerdə saat qurşağını Bakı vaxtına təyin edirik
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Baku"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DebitCopybookApplication.class, args);
 	}
