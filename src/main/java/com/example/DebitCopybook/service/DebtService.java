7afd8abc5c41444334611f8d60132c3a325cf4f4
Bilal Ibrahimov <ibrahimovbilal9@gmail.com>
2025-10-25 07:32:45 +0400

    aciqlama mesaji

--- src/main/java/com/example/DebitCopybook/service/DebtService.java
+++ src/main/java/com/example/DebitCopybook/service/DebtService.java
@@ -164,7 +164,7 @@ public class DebtService {
         // deməli bu, köhnə versiyadandır (null, boş və ya başqa bir mətn fərq etməz).
         if (!MY_DEBT_VALUE.equals(description) && !DEBT_TO_ME_VALUE.equals(description)) {
             // Bu halda, biz onun dəyərini məcburi şəkildə standart dəyərimizlə əvəz edirik.
-            requestDto.setDescription("Zəhmət olmasa play marketdən tətbiqi yeniləyin");
+            requestDto.setDescription("Zəhmət olmasa play marketdə yenilenme olub-olmadigini yoxlayin");
         }
         // Əgər gələn description bizim dəyərlərdən biridirsə, if bloku işləmir və dəyər olduğu kimi qalır.
 
@@ -499,7 +499,7 @@ public class DebtService {
             // deməli bu köhnə versiyadan gələn bir dəyərdir (boş, null və ya başqa bir mətn fərq etməz).
             // Bu halda, databazamızı təmiz saxlamaq üçün onu standart dəyərimizlə əvəz edirik.
             if (!MY_DEBT_VALUE.equals(newDescription) && !DEBT_TO_ME_VALUE.equals(newDescription)) {
-                requestDto.setDescription("Zəhmət olmasa play marketdən tətbiqi yeniləyin!!!!!!");
+                requestDto.setDescription("Zəhmət olmasa play marketdə yenilenme olub-olmadigini yoxlayin");
             }
         }
         // Əgər DTO-da description sahəsi heç göndərilməyibsə (null-dırsa), ona toxunmuruq. Bu o deməkdir ki,
