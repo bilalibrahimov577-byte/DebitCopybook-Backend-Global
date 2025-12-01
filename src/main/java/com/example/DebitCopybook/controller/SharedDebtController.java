e74f652d12f6320b92a6b9b6bcd7cab189bb0d12
Bilal Ibrahimov <bilalibrahimov577@gmail.com>
2025-12-01 14:37:07 +0400

    slash isaresini debt controllerde yazdiq

--- src/main/java/com/example/DebitCopybook/controller/SharedDebtController.java
+++ src/main/java/com/example/DebitCopybook/controller/SharedDebtController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/shared-debts") // URL-ləri ayırdıq
+@RequestMapping("/api/v1/shared-debts") // URL-ləri ayırdıq
 @RequiredArgsConstructor
 @Tag(
         name = "Qarşılıqlı Borc Controller",
