85be30196448069341edc7e499c10ad5e91dbb46
Bilal Ibrahimov <bilalibrahimov577@gmail.com>
2026-03-23 18:21:26 +0400

    isle sen cani deploy ol

--- src/main/java/com/example/DebitCopybook/config/GooglePublisherConfig.java
+++ src/main/java/com/example/DebitCopybook/config/GooglePublisherConfig.java
@@ -1,39 +1,46 @@
 package com.example.DebitCopybook.config;
 
+
+
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.androidpublisher.AndroidPublisher;
 import com.google.api.services.androidpublisher.AndroidPublisherScopes;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 
+@Slf4j
 @Configuration
 public class GooglePublisherConfig {
 
     @Value("${spring.application.name}")
     private String applicationName;
 
-    // Render-də təyin etdiyimiz environment variable-dan yolu oxuyuruq
-    @Value("${GOOGLE_APPLICATION_CREDENTIALS}")
-    private String credentialsPath;
+    @Value("${google.application-credentials}")
+    private String credentialsSource;
 
     @Bean
     public AndroidPublisher androidPublisher() throws IOException, GeneralSecurityException {
-        File file = new File(credentialsPath);
+        // MAL BAZARI proyektindəki kimi konkret yolu bura yazırıq
+        String filePath = "/etc/secrets/google-key.json";
+
+        File file = new File(filePath);
         if (!file.exists()) {
-            throw new FileNotFoundException("Fayl tapılmadı! Baxılan yol: " + file.getAbsolutePath());
+            throw new FileNotFoundException("Google JSON faylı tapılmadı! Baxılan yol: " + file.getAbsolutePath());
         }
 
         InputStream credentialsStream = new FileInputStream(file);
+
         GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream)
-               // .createScoped(Collections.singleton(AndroidPublisherScopes.ANDROID_PUBLISHER));
                 .createScoped(Collections.singleton(AndroidPublisherScopes.ANDROIDPUBLISHER));
 
         return new AndroidPublisher.Builder(
